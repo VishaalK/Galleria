f5449b0bbea3fd3457f2595041dac53e5048bcdf
Spencer <tigerragexd@gmail.com>
2015-04-20 05:39:18 -0400

    Added picture retrieving from MVP 1 Added GPS search logging TODO: GPS search?

--- app/src/main/java/com/rsv/galleria/MainActivity.java
+++ app/src/main/java/com/rsv/galleria/MainActivity.java
@@ -11,10 +11,10 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -29,13 +29,51 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
-    final Uri sourceUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//    final Uri sourceUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
     private static final Integer numViews = 3;
 
@@ -110,6 +148,22 @@ public class MainActivity extends Activity {
         return ((pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) ? true
                 : false;
     }
+
+
+
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    //define source of MediaStore.Images.Media, internal or external storage
+    final Uri sourceUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    final Uri thumbUri = MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI;
+    final String thumb_DATA = MediaStore.Images.Thumbnails.DATA;
+    final String thumb_IMAGE_ID = MediaStore.Images.Thumbnails.IMAGE_ID;
+
+    //SimpleCursorAdapter mySimpleCursorAdapter;
+    MyAdapter mySimpleCursorAdapter;
+
+    GridView myGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -223,6 +277,81 @@ public class MainActivity extends Activity {
 
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        ArrayList<Uri> fileList = new ArrayList<Uri>();
+        String[] proj = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+        ContentResolver crap = getContentResolver();
+        Cursor actualimagecursor = crap.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, proj,
+                null, null, MediaStore.Images.Media.DATE_ADDED);
+
+        String[] projection = {
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.LATITUDE,
+            MediaStore.Images.Media.LONGITUDE
+        };
+        String selection = "latitude IS NOT NULL AND longitude IS NOT NULL";
+        String[] selectionArgs = null;
+        actualimagecursor = crap.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
+                selection, selectionArgs, null);
+
+        int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+
+        for ( int i = 0 ; i < actualimagecursor.getCount() ; i++ )
+        {
+            actualimagecursor.moveToPosition(i);
+            String fileName = actualimagecursor.getString(actual_image_column_index);
+            Uri uripic = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, fileName);
+
+            Log.v("GPS", "ID: "+actualimagecursor.getInt(
+                    actualimagecursor.getColumnIndex(MediaStore.Images.Media._ID)));
+            Log.v("GPS", "Long: "+actualimagecursor.getDouble(
+                    actualimagecursor.getColumnIndex(MediaStore.Images.Media.LONGITUDE)));
+            Log.v("GPS", "Lat: "+actualimagecursor.getDouble(
+                    actualimagecursor.getColumnIndex(MediaStore.Images.Media.LATITUDE)));
+
+
+            fileList.add(( uripic));
+            Log.v(TAG, fileName);
+        }
+
+
+
+        gridview.setAdapter(new ImageAdapter(this, initialImages.toArray(new Integer[initialImages.size()])));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            private PopupWindow cachedWindow = new PopupWindow();
+
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        String[] from = {MediaStore.MediaColumns.TITLE};
+        int[] to = {android.R.id.text1};
+
+        CursorLoader cursorLoader = new CursorLoader( this, sourceUri, null, null, null,
+                MediaStore.Audio.Media.TITLE);
+
+        Cursor cursor = cursorLoader.loadInBackground();
+
+        mySimpleCursorAdapter = new MyAdapter( this, android.R.layout.simple_list_item_1, cursor,
+                from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        gridview.setAdapter(mySimpleCursorAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = mySimpleCursorAdapter.getCursor();
+                cursor.moveToPosition(position);
+
+                int int_ID = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.Media._ID));
+                getThumbnail(int_ID);
             }
         });
 
@@ -556,5 +685,109 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public class MyAdapter extends SimpleCursorAdapter {
+
+        Cursor myCursor;
+        Context myContext;
+
+        public MyAdapter(Context context, int layout, Cursor c, String[] from,
+                         int[] to, int flags) {
+            super(context, layout, c, from, to, flags);
+
+            myCursor = c;
+            myContext = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            if (row == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                row = inflater.inflate(R.layout.row, parent, false);
+            }
+
+            ImageView thumbV = (ImageView) row.findViewById(R.id.thumb);
+
+            myCursor.moveToPosition(position);
+
+            int myID = myCursor.getInt(myCursor.getColumnIndex(MediaStore.Images.Media._ID));
+
+            String[] thumbColumns = {thumb_DATA, thumb_IMAGE_ID};
+            CursorLoader thumbCursorLoader = new CursorLoader(
+                    myContext,
+                    thumbUri,
+                    thumbColumns,
+                    thumb_IMAGE_ID + "=" + myID,
+                    null,
+                    null);
+            Cursor thumbCursor = thumbCursorLoader.loadInBackground();
+
+            Bitmap myBitmap = null;
+            if (thumbCursor.moveToFirst()) {
+                int thCulumnIndex = thumbCursor.getColumnIndex(thumb_DATA);
+                String thumbPath = thumbCursor.getString(thCulumnIndex);
+                myBitmap = BitmapFactory.decodeFile(thumbPath);
+                thumbV.setImageBitmap(myBitmap);
+            }
+
+            return row;
+        }
+    }
+
+    private Bitmap getThumbnail(int id){
+
+        String[] thumbColumns = {thumb_DATA, thumb_IMAGE_ID};
+
+        CursorLoader thumbCursorLoader = new CursorLoader(
+                this,
+                thumbUri,
+                thumbColumns,
+                thumb_IMAGE_ID + "=" + id,
+                null,
+                null);
+
+        Cursor thumbCursor = thumbCursorLoader.loadInBackground();
+
+        Bitmap thumbBitmap = null;
+        if(thumbCursor.moveToFirst()){
+            int thCulumnIndex = thumbCursor.getColumnIndex(thumb_DATA);
+
+            String thumbPath = thumbCursor.getString(thCulumnIndex);
+
+            Toast.makeText(getApplicationContext(),
+                    thumbPath,
+                    Toast.LENGTH_LONG).show();
+
+            thumbBitmap = BitmapFactory.decodeFile(thumbPath);
+
+            //Create a Dialog to display the thumbnail
+            AlertDialog.Builder thumbDialog = new AlertDialog.Builder(MainActivity.this);
+            ImageView thumbView = new ImageView(MainActivity.this);
+            thumbView.setImageBitmap(thumbBitmap);
+            LinearLayout layout = new LinearLayout(MainActivity.this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            layout.addView(thumbView);
+            thumbDialog.setView(layout);
+            thumbDialog.show();
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "NO Thumbnail!",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        return thumbBitmap;
     }
 }
